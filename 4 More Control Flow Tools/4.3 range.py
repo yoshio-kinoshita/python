13ba39cb9b99d97cf933372ed32ebd33067e2f36
Yoshio Kinoshita <yoshiokinoshita@icloud.com>
2018-04-05 14:32:30 +0900

    Add new py files

--- 4 More Control Flow Tools/4.3 range.py
+++ 4 More Control Flow Tools/4.3 range.py
@@ -1,2 +1,15 @@
 for i in range(5):
     print(i)
+    
+print(range(5, 10))
+
+print(range(0, 10, 3))
+
+print(range(-10, -100, -30))
+
+a = ['Mary' 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i,a[i])
+
+print(range(10))
+print(list(range(10)))
